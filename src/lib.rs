42ab254efb228e268256d463ff2e75656231f4d8
Mubelotix <unbeliever.in.time@gmail.com>
2019-12-26 16:38:00 +0100

    added a description clearer

--- src/lib.rs
+++ src/lib.rs
@@ -30,10 +30,26 @@ mod string_tools {
         Some(&text[end..])
     }
 
+    pub fn get_idx_before_strict<'a>(text: &'a str, begin: &str) -> Option<usize> {
+        let begin = text.find(begin)?;
+        Some(begin)
+    }
+
+    pub fn get_idx_after_strict<'a>(text: &'a str, end: &str) -> Option<usize> {
+        let end = text.find(end)? + end.len();
+        Some(end)
+    }
+
     pub fn get_all_between_strict<'a>(text: &'a str, begin: &str, end: &str) -> Option<&'a str> {
         let text = get_all_after_strict(text, begin)?;
         let text = get_all_before_strict(text, end)?;
         Some(text)
+    }
+
+    pub fn get_idx_between_strict<'a>(text: &'a str, begin: &str, end: &str) -> Option<(usize, usize)> {
+        let after = get_idx_after_strict(text, begin)?;
+        let before = get_idx_before_strict(&text[after..], end)?;
+        Some((after, after + before))
     }
 
     pub fn get_all_before<'a>(text: &'a str, begin: &str) -> &'a str {
@@ -191,5 +207,164 @@ pub mod intermediary {
 
 /// Empty for now
 pub mod gleam {
+    use super::string_tools::get_all_between_strict;
+    use super::string_tools::get_idx_between_strict;
+    use std::time::{SystemTime, UNIX_EPOCH, Duration};
+    use std::thread::sleep;
 
+    fn clear_description(description: &mut String) {
+        while let Some((x, x2)) = get_idx_between_strict(&description, "\\u003c", "\\u003e") {
+            let mut before = description[..x-6].to_string();
+            let after = &description[x2+6..];
+            before.push_str(after);
+            *description = String::from(before);
+        }
+    }
+
+    /// A simple struct used to store informations about a gleam.io giveaway.
+    #[derive(Debug)]
+    pub struct Giveaway {
+        url: String,
+        entry_count: u64,
+        start_date: u64,
+        end_date: u64,
+        update_date: u64,
+        name: String,
+        description: String,
+    }
+
+    impl Giveaway {
+        /// Load a gleam.io page and produce a giveaway struct. Return None if something don't work
+        pub fn fetch(url: &str) -> Option<Giveaway> {
+            if let Ok(response) = minreq::get(url)
+                .with_header("Host", "gleam.io")
+                .with_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0")
+                .with_header("Accept", "text/html")
+                .with_header("DNT", "1")
+                .with_header("Connection", "keep-alive")
+                .with_header("Upgrade-Insecure-Requests", "1")
+                .with_header("TE", "Trailers")
+                .send()
+            {
+                let body: &str = &response.body;
+
+                let start_date: u64 = if let Ok(start_date) = get_all_between_strict(body, "starts_at&quot;:", ",&quot;")?.parse() {
+                    start_date
+                } else {
+                    return None;
+                };
+                let end_date: u64 = if let Ok(end_date) = get_all_between_strict(body, "ends_at&quot;:", ",&quot;")?.parse() {
+                    end_date
+                } else {
+                    return None;
+                };
+                let entry_count: u64 = if let Ok(entry_count) = get_all_between_strict(body, "initEntryCount(", ")")?.parse() {
+                    entry_count
+                } else {
+                    return None;
+                };
+                let name = get_all_between_strict(body, "name&quot;:&quot;", "&quot;")?.to_string();
+                let mut description = get_all_between_strict(body, "description&quot;:&quot;", "&quot;")?.to_string();
+                clear_description(&mut description);
+
+                Some(Giveaway {
+                    url: url.to_string(),
+                    description,
+                    entry_count,
+                    start_date,
+                    end_date,
+                    update_date: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+                    name
+                })
+            } else {
+                None
+            }
+        }
+
+        /// Fetch some urls and wait a cooldown between each request
+        pub fn fetch_vec(urls: Vec<&str>, cooldown: u64) -> Vec<Giveaway> {
+            let mut giveaways = Vec::new();
+
+            for url in &urls {
+                if let Some(giveaway) = Giveaway::fetch(url) {
+                    giveaways.push(giveaway)
+                }
+                if urls.len() > 1 {
+                    sleep(Duration::from_secs(cooldown));
+                }
+            }
+
+            giveaways
+        }
+
+        /// Return the url
+        pub fn get_url(&self) -> &str {
+            &self.url
+        }
+
+        /// Return the name
+        pub fn get_name(&self) -> &str {
+            &self.name
+        }
+
+        /// Return the description
+        pub fn get_description(&self) -> &str {
+            &self.description
+        }
+
+        /// Return the number of entries
+        pub fn get_entry_count(&self) -> u64 {
+            self.entry_count
+        }
+
+        /// Return the creation date in seconds
+        pub fn get_start_date(&self) -> u64 {
+            self.start_date
+        }
+
+        /// Return the end date in seconds
+        pub fn get_end_date(&self) -> u64 {
+            self.end_date
+        }
+
+        /// Return the last update date in seconds
+        pub fn get_update_date(&self) -> u64 {
+            self.update_date
+        }
+
+        /// Check if the giveaway is ended
+        pub fn is_active(&self) -> bool {
+            if self.end_date < SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() {
+                return true;
+            }
+            return false;
+        }
+
+        /// Reload the giveaway and update informations
+        pub fn update(&mut self) {
+            if let Some(giveaway) = Giveaway::fetch(&self.url) {
+                *self = giveaway;
+            } else {
+                eprintln!("this giveaways seems to be inexistant now...");
+            }
+        }
+    }
+
+    #[test]
+    fn test_giveaway_struct() {
+        let giveaway = Giveaway::fetch("https://gleam.io/WUlv8/troy-xmas-giveaway").unwrap();
+        println!("{:?}", giveaway);
+
+        sleep(Duration::from_secs(5));
+
+        let giveaway = Giveaway::fetch("https://gleam.io/8nTqy/amd-5700xt-gpu").unwrap();
+        println!("{:?}", giveaway);
+    }
+
+    #[test]
+    fn test_description() {
+        let mut description = String::from("\\u003ch2\\u003eRetweet to Win Giveaway of $1000 TROY Tokens!\\u003c/h2\\u003e");
+        clear_description(&mut description);
+        println!("{}", description);
+    }
 }
